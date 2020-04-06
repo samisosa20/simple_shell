0a12eb4a637ad68216acf45445a474b834c1c014
isaza00 <isaza00@hotmail.com>
2020-04-06 18:58:51 -0500

    fix leaks

--- shell.c
+++ shell.c
@@ -1,38 +1,17 @@
 #include "shell.h"
 /**
- * _strlen - A function to count the lenght of an array
- * @s : array
- * Return: an int of the lenght of an array
+ * _strlen - isdigit
+ * @s: integer
+ * Return: prints alphabet
  */
-
 int _strlen(char *s)
 {
-	int m = 0;
 	int i;
+	int len;
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
-		m = m + 1;
-	}
-	return (m);
-}
-/**
- * *_strcpy - Return the copy of an string
- * @dest : an array
- * @src : an array
- * Return: an array
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-	return (dest);
+		len++;
+	return (len);
 }
 /**
  * get_flags - basic shell
@@ -40,55 +19,20 @@ char *_strcpy(char *dest, char *src)
  * @bytes_read: size of string
  * Return: prints in screen $ and received commands
  */
-char **get_flags(char *string, int size)
+void get_flags(char **argv, char *string, ssize_t bytes_read)
 {
-	char **arguments;
+	char **arguments = argv;
 	int i = 0, j = 0;
-	char *token;
-	char copy[100];
+	char *token = strtok(string, " ");
 
-	printf("%s\n", _strcpy(copy, string));
-	token = strtok(copy, " ");
 	while (token != NULL)
 	{
-		token = strtok(NULL, " ");
-		j++;
-	}
-	token = strtok(string, " ");
-	arguments = malloc(sizeof(char *) * size);
-	while (token != NULL)
-	{		
-		arguments[i] = malloc(sizeof(char) * _strlen(token));
 		arguments[i] = token;
-		printf("token get_flags = %s\n", token);
 		token = strtok(NULL, " ");
 		i++;
 	}
 	arguments[i] = NULL;
 	arguments[i - 1] = strtok(arguments[i - 1], NEW_LINE);
-	return (arguments);
-}
-/**
- * get_flags - basic shell
- * @string: Pointer to string
- * @bytes_read: size of string
- * Return: prints in screen $ and received commands
- */
-int _size(char *string)
-{
-	int j = 0;
-	char *token;
-	char copy[1024];
-
-	_strcpy(copy, string);
-	token = strtok(copy, " ");
-	while (token != NULL)
-	{
-		printf("token size = %s\n", token);
-		token = strtok(NULL, " ");
-		j++;
-	}	
-	return (j);
 }
 /**
  * main - basic shell
@@ -102,51 +46,57 @@ int main(void)
 	size_t size = 0;
 	ssize_t bytes_read = 0;
 	char *string = NULL;
-	int i = 0, j = 0;
+	int i = 0;
+	pid_t parent;
 
 	while (1)
 	{
-		
 		printf("$ ");
-		string = (char *)malloc(size);
-		if (string == NULL)
-		{
-			free(string);
-			continue;
-		}
 		bytes_read = getline(&string, &size, stdin);
 		if (bytes_read == -1)
+			exit(0);
+		if (*string == '\n' || *string == ' ')
 		{
-			free(string);
-			exit(0); /*recordar que aquí debe haber un posible perror*/
+			continue;
 		}
-		j = _size(string);
-		argv = get_flags(string, j);
+		argv = malloc(sizeof(char *));
+		if (!argv)
+			exit(0);
+		for (i = 0; argv[i]; i++)
+		{
+			argv[i] = malloc(sizeof(char) * _strlen(argv[i]) + 1);
+			if (!argv[i])
+				free(argv), exit(0);
+		}
 		my_pid = fork();
 		if (my_pid == 0)
 		{
-			
-			if (*string == '\n' || *string == ' ')
-			{
-				free(string);
-				break;
-			}
+			parent = getppid();
+			get_flags(argv, string, bytes_read);
 			if (execve(argv[0], argv, NULL) == -1)
 			{
+				for (i = 0; argv[i]; i++)
+					free(argv[i]);
+				free(argv);
 				perror("Error2");
 			}
+			exit(0);
 		}
 		else if (my_pid > 0)
-			wait(NULL);
-		else
-			perror("Error3");
-		for (; j >= 0; j--)
 		{
-			printf("final = %s\n", argv[j]);
-			//free(argv[j]);
+			wait(0);
+			for (i = 0; argv[i]; i++)
+				free(argv[i]);
+			free(argv);
 		}
-		free(argv);
-		free(string);
+		else
+		{
+			for (i = 0; argv[i]; i++)
+				free(argv[i]);
+			free(argv);
+			perror("Error3");
+			exit(0);
+		}
 	}
 	return (0);
 }
